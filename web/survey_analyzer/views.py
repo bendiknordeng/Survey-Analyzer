54ddeb35f0ade0a46175d92e98e498f436dd0225
Bendik Nordeng <bendiknor@gmail.com>
2020-04-01 08:34:03 +0200

    Working drag and drop

--- web/survey_analyzer/views.py
+++ web/survey_analyzer/views.py
@@ -5,18 +5,16 @@ from django.conf import settings
 from .source.file_converter import Converter
 
 def home(request):
-    return render(request, 'home.html')
-
-def define_structure(request):
-    context = {}
     if request.method == 'POST':
         uploaded_file = request.FILES['survey']
         file_storage = FileSystemStorage()
         path = file_storage.save(uploaded_file.name, uploaded_file)
         paths = Converter(settings.MEDIA_ROOT+path).save(settings.MEDIA_ROOT)
         default_storage.delete(path)
-        context['example_img'] = paths[0]
-    return render(request, 'define_structure.html', context)
+    return render(request, 'home.html')
+
+def define_structure(request):
+    return render(request, 'define_structure.html')
 
 def analyze(request):
     return render(request, 'analyze.html')
